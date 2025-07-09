51edb6b36394c232b465301d9c66a26a4ea59fcf
Jason Keeler <jase@metalhead.co.uk>
2025-07-09 17:23:45 +0100

    Added unit tests for Marketstack.

---
+++ Metalhead.SharesGainLossTracker.Core.Tests/MarketstackMockData.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace Metalhead.SharesGainLossTracker.Core.Tests;
+
+internal static class MarketstackMockData
+{
+    internal static HttpResponseMessage CreateMarketstackAccessRestrictedHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"function_access_restricted\",\"message\":\"Your current subscription plan does not support this API function\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackRateLimitHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.TooManyRequests)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"rate_limit_reached\",\"message\":\"You have exceeded the maximum rate limitation allowed on your subscription plan. Please refer to the \\\"Rate Limits\\\" section of the API Documentation for details.\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackMonthlyLimitHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.TooManyRequests)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"usage_limit_reached\",\"message\":\"Your monthly usage limit has been reached. Please upgrade your Subscription Plan.\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackTooManyRequestsHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.TooManyRequests)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"too_many_requests\",\"message\":\"\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackInvalidEndpointHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.NotFound)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"invalid_api_function\",\"message\":\"The API function you requested does not exist or is not supported.\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackNoValidSymbolsHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.UnprocessableContent)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"no_valid_symbols_provided\",\"message\":\"At least one valid symbol must be provided\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackNotFoundHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.NotFound)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"not_found_error\",\"message\":\"Route not found\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackOtherErrorHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.UnprocessableEntity)
+        {
+            Content = new StringContent("{\"error\":{\"code\":\"some-other-marketstack-error\",\"message\":\"\"}}", Encoding.UTF8, "application/json")
+        };
+    }
+
+    internal static HttpResponseMessage CreateMarketstackDeserializingErrorHttpResponse()
+    {
+        return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+        {
+            Content = new StringContent("{\"InvalidJson\":\"This is not a valid JSON response\"}", Encoding.UTF8, "application/json")
+        };
+    }
+}
---
+++ Metalhead.SharesGainLossTracker.Core.Tests/MarketstackTests.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+using Metalhead.SharesGainLossTracker.Core.Models;
+
+namespace Metalhead.SharesGainLossTracker.Core.Tests;
+
+public class MarketstackTests
+{
+    private readonly Mock<ILogger<Marketstack>> _mockLogger = new();
+    private readonly Mock<IProgress<ProgressLog>> _mockProgress = new();
+    private readonly Marketstack _sut;
+
+    public MarketstackTests()
+    {
+        _sut = new Marketstack(_mockLogger.Object, _mockProgress.Object);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenRateLimitExceededError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackRateLimitHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Rate limit exceeded error from stocks API.  Try increasing the ApiDelayPerCallMilliseconds setting.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Rate limit exceeded error from stocks API.  Try increasing the ApiDelayPerCallMilliseconds setting.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenMonthlyLimitExceededError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackMonthlyLimitHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Monthly API calls limit reached error from stocks API.  Plans with a higher limit may be available.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Monthly API calls limit reached error from stocks API.  Plans with a higher limit may be available.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenTooManyRequestsError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackTooManyRequestsHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Monthly API calls limit reached error from stocks API.  Plans with a higher limit may be available.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Monthly API calls limit reached error from stocks API.  Plans with a higher limit may be available.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenAccessRestrictedError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackAccessRestrictedHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Access restricted error from stocks API.  Your plan may need upgrading to use this API endpoint.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Access restricted error from stocks API.  Your plan may need upgrading to use this API endpoint.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenInvalidEndpointError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackInvalidEndpointHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Invalid endpoint error from stocks API.  Verify the endpoint URL is correct, especially the stock symbol.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Invalid endpoint error from stocks API.  Verify the endpoint URL is correct, especially the stock symbol.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenNotFoundError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackNotFoundHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Invalid endpoint error from stocks API.  Verify the endpoint URL is correct, especially the stock symbol.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Invalid endpoint error from stocks API.  Verify the endpoint URL is correct, especially the stock symbol.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenNoValidSymbolsError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackNoValidSymbolsHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Invalid stock symbol error from stocks API.  Verify the stock symbols in the input file.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Invalid stock symbol error from stocks API.  Verify the stock symbols in the input file.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenDeserializingError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackDeserializingErrorHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Error deserializing stocks data.  Try increasing the ApiDelayPerCallMilliseconds setting.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Error deserializing stocks data.  Try increasing the ApiDelayPerCallMilliseconds setting.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetStocksDataAsync_WhenOtherError_LogsAndReportsError()
+    {
+        // Arrange
+        ProgressLog? reportedLog = null;
+        _mockProgress.Setup(p => p.Report(It.IsAny<ProgressLog>()))
+            .Callback<ProgressLog>(log => reportedLog = log);
+        var httpResponse = MarketstackMockData.CreateMarketstackOtherErrorHttpResponse();
+        var responses = new[] { httpResponse };
+
+        // Act
+        var result = await ((IStock)_sut).GetStocksDataAsync(responses, false);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Equals("Unknown error from stocks API.")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        Assert.NotNull(reportedLog);
+        Assert.Equal(MessageImportance.Bad, reportedLog.Importance);
+        Assert.Equal("Unknown error from stocks API.", reportedLog.DownloadLog);
+        Assert.Empty(result);
+    }
+}
